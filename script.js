f9b714e68910ed6cdf7cd20843016290665ce6b3
ThaissaMariana <thaissa2608mariana@gmail.com>
2023-05-03 13:45:17 -0400

    Commit modificado

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // divListaProduto.insertAdjacentHTML('afterbegin', '');
 class Produto {
-    constructor(nome, datadecadastro, descricao, preco){
+    constructor(nome, data_cadastro, descricao, preco){
         this.nome = nome;
-        this.datadecadastro = datadecadastro;
+        this.data_cadastro = data_cadastro;
         this.descricao = descricao;
         this.preco = preco;
     }
 
     mostrar_dados(){
-        return this.nome +  this.datadecadastro + this.descricao + this.preco;
+        return this.nome +  this.data_cadastro + this.descricao + this.preco;
     }
 }
 
-class ProdutoDestaque extends Produto{
-        constructor(nome, datadecadastro , inscricao, preco, produto_destaque){
-            super(nome, datadecadastro, inscricao, preco );
+class Produto_Destaque extends Produto{
+        constructor(nome, data_cadastro , inscricao, preco, produto_destaque){
+            super(nome, data_cadastro, inscricao, preco );
             this.produto_destaque = produto_destaque;
         }
-        mostrarProdutoDestaque(){
-            return this.nome + this.datadecadastro + this.descricao + this.preco + this.produto_destaque
+        mostrarProduto_Destaque(){
+            return this.nome + this.data_cadastro + this.descricao + this.preco + this.produto_destaque
         }
 }
+
